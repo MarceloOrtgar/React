8721bbd3aedc6e2c5fd2c9bdc5c9486d3856e827
Marcelo <marceloortega18@gmail.com>
2022-10-27 22:36:12 -0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// import { HooksApp } from "./HooksApp";
+// import { CounterApp } from "./01-useState/CounterApp";
+// import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+// import { SmpleForm } from "./02-useEffect/SmpleForm";
+// import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+    <FocusScreen />
+  /* </React.StrictMode> */
+);
