79f9645af035ab661df3b5ffaaac6934a8a6a03d
Marcelo <marceloortega18@gmail.com>
2022-10-28 19:56:57 -0400

    Seccion * culminada

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,8 +1,32 @@
-export const Quote = ({author, quote}) => {
+import { useLayoutEffect, useRef, useState } from "react";
+
+export const Quote = ({ author, quote }) => {
+  const pRef = useRef();
+  const [boxSize, setboxSize] = useState({ width: 0, height: 0 });
+
+  useLayoutEffect(() => {
+    const { width, height } = pRef.current.getBoundingClientRect();
+    setboxSize({
+      width,
+      height,
+    });
+  }, [quote]);
+
   return (
-      <blockquote className="blockquote text-end">
-        <p className="mb-1">{quote}</p>
+    <>
+      <blockquote
+        className="blockquote text-end"
+        style={{
+          display: "flex",
+        }}
+      >
+        <p ref={pRef} className="mb-1">
+          {quote}
+        </p>
         <footer className="blockquote-footer"> {author} </footer>
       </blockquote>
+
+      <code>{JSON.stringify(boxSize)}</code>
+    </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,18 @@ import ReactDOM from "react-dom/client";
 // import { SmpleForm } from "./02-useEffect/SmpleForm";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
-import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { Layout } from "./05-useLayoutEffect/Layout";
+// import { Memorize } from "./06-memos/Memorize";
+// import { MemoHook } from "./06-memos/MemoHook";
+// import { CallBackHook } from "./06-memos/CallBackHook";
+import { Padre } from "./07-tarea-memo/Padre";
+
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <FocusScreen />
+    <Padre />
   /* </React.StrictMode> */
 );
